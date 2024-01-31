7fc14324b91aeba2657ddb92535c8505fceaab72
Thiago <thgps01@outlook.com>
2024-01-30 22:43:10 -0300

    feat: add AuthProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
 import './globals.css'
 import { Header } from '@/components/header'
+import AuthProvider from './providers/auth'
 
 const nunito = Nunito({ subsets: ['latin'] })
 
@@ -18,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${nunito.className} dark`}>
-        <Header />
-        {children}
+        <AuthProvider>
+          <Header />
+          {children}
+        </AuthProvider>
       </body>
     </html>
   )
