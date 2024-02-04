04f5e8ba3f90207ba0553e5cb2f202a402b02221
Thiago <thgps01@outlook.com>
2024-02-04 15:48:48 -0300

    fix: ensure that past times are not returned

--- src/app/barbershops/[id]/_helpers/hours.ts
+++ src/app/barbershops/[id]/_helpers/hours.ts
@@ -1,16 +1,19 @@
-import { addMinutes, format, setHours, setMinutes } from 'date-fns'
+import { addMinutes, format, isAfter, setHours, setMinutes } from 'date-fns'
 
 export function generateDayTimeList(date: Date): string[] {
+  const currentTime = new Date()
   const startTime = setMinutes(setHours(date, 9), 0)
   const endTime = setMinutes(setHours(date, 21), 0)
   const interval = 45
   const timeList: string[] = []
 
-  let currentTime = startTime
+  let currentTimeSlot = startTime
 
-  while (currentTime <= endTime) {
-    timeList.push(format(currentTime, 'HH:mm'))
-    currentTime = addMinutes(currentTime, interval)
+  while (currentTimeSlot <= endTime) {
+    if (isAfter(currentTimeSlot, currentTime)) {
+      timeList.push(format(currentTimeSlot, 'HH:mm'))
+    }
+    currentTimeSlot = addMinutes(currentTimeSlot, interval)
   }
 
   return timeList
